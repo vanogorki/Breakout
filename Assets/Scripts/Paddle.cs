5757cfdc1eed1c2e5bd69cb3a087cc3a1344a4a9
vanogorki <vanogorki@gmail.com>
2024-02-22 17:42:35 +0400

    add inputs

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
+    /*private PlayerInput _playerInput;
+    private InputAction _inputAction;*/
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        //_playerInput = GetComponent<PlayerInput>();
+        //_inputAction = _playerInput.actions["MousePostion"];
     }
 
     // Update is called once per frame
