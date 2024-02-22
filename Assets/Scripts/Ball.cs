3a32a4f94865242c3f98383f96bbf6c93053da93
vanogorki <vanogorki@gmail.com>
2024-02-22 11:58:19 +0400

    add colliders

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] private float _speed = 5.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = Vector2.up * 5;
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * _speed;
     }
 
     // Update is called once per frame
