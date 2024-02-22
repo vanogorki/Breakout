5fdcfed1d32d6580051bdd103de0ac69f2e20cb2
vanogorki <vanogorki@gmail.com>
2024-02-22 22:03:48 +0400

    spawn objects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,11 +9,20 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject _brickPrefab;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        _paddlePrefab.SetActive(true);
-        _ballPrefab.SetActive(true);
-        _brickPrefab.SetActive(true);
+        // spawn paddle
+        Instantiate(_paddlePrefab, new Vector3(0, -4f, 0), Quaternion.identity);
+        // spawn ball
+        Instantiate(_ballPrefab, new Vector3(0, -3.9f, 0), Quaternion.identity);
+        // spawn bricks
+        for (float i = 0; i < 5f; i += 0.5f)
+        {
+            for (float j = 0; j < 16.5f; j += 1.5f)
+            {
+                Instantiate(_brickPrefab, new Vector3(j - 7.5f, 4f - i, 0), Quaternion.identity);
+            }
+        }        
     }
 
     // Update is called once per frame
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,23 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.InputSystem;
+using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
-    /*private PlayerInput _playerInput;
-    private InputAction _inputAction;*/
-    
+    private PlayerInput _playerInput;
+    private InputAction _inputAction;
+    private Camera _camera;
+    private float _paddleHalfWidth;
+    private float _cameraHalfWidth;
+
     // Start is called before the first frame update
     void Start()
     {
-        //_playerInput = GetComponent<PlayerInput>();
-        //_inputAction = _playerInput.actions["MousePostion"];
+        _playerInput = GetComponent<PlayerInput>();
+        _inputAction = _playerInput.actions["MousePosition"];
+        _camera = Camera.main;
+        _paddleHalfWidth = GetComponent<Collider2D>().bounds.size.x / 2;
+        _cameraHalfWidth = _camera.orthographicSize * _camera.aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 mousePos = _camera.ScreenToWorldPoint(_inputAction.ReadValue<Vector2>());
+        transform.position = new Vector2(mousePos.x, transform.position.y);
         
+        // check if the paddle is within camera bounds
+        float x = Mathf.Clamp(transform.position.x, -_cameraHalfWidth + _paddleHalfWidth, _cameraHalfWidth - _paddleHalfWidth);
+        transform.position = new Vector2(x, transform.position.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            // calculate where the ball hit the paddle and change the direction of the ball
+            Vector2 hitPoint = other.GetContact(0).point;
+            Vector2 paddleCenter = new Vector2(transform.position.x, transform.position.y);
+            Vector2 direction = hitPoint - paddleCenter;
+            var speed = other.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = direction.normalized * speed;
+        }
     }
 }
